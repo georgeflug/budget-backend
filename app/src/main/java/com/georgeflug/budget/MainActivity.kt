02650b5d19baea510e1cb18c7a1efda948e25ec9
Richard White <georgeflug@gmail.com>
2018-09-16 20:14:59 -0500

    Wire up navigation to each tab

--- app/src/main/java/com/georgeflug/budget/MainActivity.kt
+++ app/src/main/java/com/georgeflug/budget/MainActivity.kt
@@ -2,10 +2,19 @@ package com.georgeflug.budget
 
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
+import android.view.View
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        bottomNav.setOnNavigationItemSelectedListener {
+            addTransactionFragmentContainer.visibility = if (it.itemId == R.id.nav_add) View.VISIBLE else View.GONE
+            transactionsFragmentContainer.visibility = if (it.itemId == R.id.nav_transactions) View.VISIBLE else View.GONE
+            budgetsFragmentContainer.visibility = if (it.itemId == R.id.nav_budgets) View.VISIBLE else View.GONE
+            true
+        }
     }
 }
