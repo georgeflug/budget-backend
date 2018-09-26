cd74b0ef92b2c266a28ab7202b414b3cdb414cdc
Richard White <georgeflug@gmail.com>
2018-09-25 22:46:05 -0500

    Go back to using non-dev url. The dev url requires you to be logged in.

--- app/src/main/java/com/georgeflug/budget/api/TransactionApi.kt
+++ app/src/main/java/com/georgeflug/budget/api/TransactionApi.kt
@@ -7,7 +7,7 @@ import io.reactivex.schedulers.Schedulers
 import java.net.URL
 
 object TransactionApi {
-    private const val url = "https://script.google.com/macros/s/AKfycbzn7I-KiMGF5N2V8xJXLJTSn1Zzh4da4G-Jjp-7RfM/dev"
+    private const val url = "https://script.google.com/macros/s/AKfycbzJXrwFepauVmiodXfe81zETyqgAMcwdjR8fRjJ1NvrcpAgPPg/exec"
 
     fun getTransactions(): Observable<TransactionApiListResult> =
             getRequest("$url?route=getTransactions").cache()
