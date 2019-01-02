54c7cd07dca35cd43b1cfb3c0ea802293c730d3a
Richard White <richard.white@asynchrony.com>
2019-01-02 17:30:03 -0600

    Dockerize the backend app

--- backend/index.js
+++ backend/index.js
@@ -1,1 +1,14 @@
-console.log("Todo: write the app!");
+var express = require('express')
+
+var app = express()
+
+app.get('/', function (req, res) {
+  res.send('Hello World')
+})
+
+app.get('/status', function (req, res) {
+  res.send('{"status":"OK"}')
+});
+
+app.listen(3000)
+console.log('Listening on localhost:3000');
