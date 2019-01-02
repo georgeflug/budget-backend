dc318a1d9c8cba1b05d75977497a810199c8770e
Richard White <richard.white@asynchrony.com>
2019-01-02 16:42:44 -0600

    Move android app into subfolder

--- app/src/main/java/com/georgeflug/budget/BudgetApplication.kt
+++
@@ -1,9 +0,0 @@
-package com.georgeflug.budget
-
-import android.app.Application
-
-class BudgetApplication : Application() {
-    override fun onCreate() {
-        super.onCreate()
-    }
-}
--- app/src/main/java/com/georgeflug/budget/api/TransactionApiResult.kt
+++
@@ -1,3 +0,0 @@
-package com.georgeflug.budget.api
-
-data class TransactionApiResult(val result: String, val error: String)
