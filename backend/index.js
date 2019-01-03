569047288b6a9eced9e5bfd907a93662fee8a8f5
Richard White <richard.white@asynchrony.com>
2019-01-03 15:35:07 -0600

    Successfully connect to mongodb running in a parallel container

--- backend/index.js
+++ backend/index.js
@@ -10,5 +10,45 @@ app.get('/status', function (req, res) {
   res.send('{"status":"OK"}')
 });
 
-app.listen(3000)
-console.log('Listening on localhost:3000');
+const MongoClient = require('mongodb').MongoClient;
+// Connection URL
+const url = 'mongodb://mongo:27017';
+
+// Database Name
+const dbName = 'budget';
+
+// Use connect method to connect to the server
+connectToDbWithRetry(function(err, client) {
+  console.log("Connected successfully to mongodb server");
+
+  const db = client.db(dbName);
+
+  app.get('/read', function (req, res) {
+    const collection = db.collection('documents');
+    collection.find().toArray(function(err, docs) {
+      res.send(docs);
+    });
+  });
+
+  app.get('/write', function (req, res) {
+    const collection = db.collection('documents');
+    collection.insertMany([{a : 1}], function(err, result) {
+      res.send(result);
+    });
+  });
+
+  app.listen(3000)
+  console.log('Listening on localhost:3000');
+});
+
+function connectToDbWithRetry(cb) {
+  console.log('Attempting to connect to mongodb');
+  MongoClient.connect(url, function(err, client) {
+    if (err) {
+      console.log('Mongodb server not ready. Retrying...');
+      setTimeout(() => connectToDbRaw(cb), 1000);  
+    } else {
+      cb(null, client);
+    }
+  });
+}
