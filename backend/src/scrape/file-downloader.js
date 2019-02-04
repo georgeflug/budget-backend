1a434ac1e6c99cdbb23d2ba0692022f8d7f90db9
Richard White <georgeflug@gmail.com>
2019-02-04 17:18:32 -0600

    Add ability to grab a file that was downloaded by chrome

---
+++ backend/src/scrape/file-downloader.js
@@ -0,0 +1,41 @@
+const fs = require('fs');
+const fsp = require('fs').promises;
+
+const homedir = require('os').homedir();
+const downloadsFolder = homedir + '/Downloads/';
+
+function downloadFileByTriggering(triggerAction) {
+  return new Promise((resolve, reject) => {
+    watchForFileAndDownloadIt(resolve, reject);
+    triggerAction();
+  });
+}
+
+function watchForFileAndDownloadIt(resolve, reject) {
+  const watcher = fs.watch(downloadsFolder, (eventType, fileName) => {
+    if (!isTemporaryChromeFile(fileName)) {
+      watcher.close();
+      setTimeout(() => {
+        downloadFile(fileName)
+          .then(file => resolve(file))
+          .catch(e => reject(e));
+      }, 1000);
+    }
+  });
+
+}
+
+function isTemporaryChromeFile(fileName) {
+  return fileName.includes('.chromium') || fileName.includes('.crdownload');
+}
+
+async function downloadFile(fileName) {
+  const path = downloadsFolder + '/' + fileName; 
+  const rawFile = await fsp.readFile(path, 'utf8');
+  await fsp.unlink(path);
+  return rawFile.toString('utf8');
+}
+
+module.exports = {
+  downloadFileByTriggering: downloadFileByTriggering
+};
