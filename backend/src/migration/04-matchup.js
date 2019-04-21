a8d5ed7294e05e4262a1b56463d33a1b86f3930e
Richard White <georgeflug@gmail.com>
2019-04-21 14:13:03 -0500

    Another migration fix. Budgets and descriptions were not being saved.

--- backend/src/migration/04-matchup.js
+++ backend/src/migration/04-matchup.js
@@ -131,11 +131,15 @@ const data = matches.map(row => Object.values(row).join('|'));
 // }
 
 require('../db/db').initDb().then(async function () {
-  const adaptTransactions = require('./adapt-transactions');
-  const saveTransactions = require('./save-transactions');
+  const saveTransactions = require('../plaid/save-transactions');
 
+  console.log("Adapting transactions...");
   const adaptedTransactions = await adaptTransactions(plaidData);
-  await saveTransactions(adaptedTransactions);
+  console.log("Saving transactions...");
+  const results = await saveTransactions(adaptedTransactions);
+  console.log("Complete!\nMetrics:");
+  console.dir(results);
+  process.exit(0);
 
   // {
   //   "account_id": "DoQmRxwVbDTw4X5wbBNyTEZ7kDN9ZRfZE880w",
@@ -176,3 +180,27 @@ require('../db/db').initDb().then(async function () {
   // },
 
 });
+
+const ACCOUNT_MAP = {
+  'o3d3dPnELRtY7gEPPaBVsbZkDqJeQLCB680A5': 'Discover',
+  'DoQmRxwVbDTw4X5wbBNyTEZ7kDN9ZRfZE880w': 'Checking',
+  '1EzgJnaKqxFD8R9D5pdXug0qLPKx0XHmJXXEY': 'Savings'
+};
+
+function adaptPlaidTransactionForDb(transactions) {
+  return transactions.map(transaction => {
+    return {
+      date: transaction.date,
+      totalAmount: transaction.amount,
+      account: ACCOUNT_MAP[transaction.account_id],
+      postedDate: transaction.date,
+      postedDescription: transaction.name,
+      plaidId: transaction.transaction_id,
+      splits: [{
+        amount: transaction.amount,
+        budget: transaction.budget,
+        description: transaction.description
+      }]
+    };
+  });
+};
