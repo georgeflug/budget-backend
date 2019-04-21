160f871bd19cfcc0e2faa3bffecf23350a50f5da
Richard White <georgeflug@gmail.com>
2019-04-20 21:09:52 -0500

    Add scripts to fix disconnected accounts

---
+++ backend/src/plaid-fix-accounts/02-create-public-token-fccu.js
@@ -0,0 +1,20 @@
+const plaid = require('plaid');
+
+const client = new plaid.Client(
+  process.env.PLAID_CLIENT_ID,
+  process.env.PLAID_SECRET,
+  process.env.PLAID_PUBLIC_KEY,
+  plaid.environments['development'],
+  {
+    version: '2018-05-22'
+  }
+);
+
+// Create a public_token for use with Plaid Link's update mode
+client.createPublicToken(process.env.FCCU_ACCESS_KEY, (err, result) => {
+  // Handle err
+  // Use the generated public_token to initialize Plaid Link
+  // in update mode for a user's Item so that they can provide
+  // updated credentials or MFA information
+  console.log(result);
+});
