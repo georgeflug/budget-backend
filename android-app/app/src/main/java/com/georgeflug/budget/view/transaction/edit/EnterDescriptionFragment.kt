05b53b146894aa24c50d0ffa6d27eec754991d2a
Richard White <georgeflug@gmail.com>
2019-04-22 11:50:49 -0500

    Hide keyboard after entering description

--- android-app/app/src/main/java/com/georgeflug/budget/view/transaction/edit/EnterDescriptionFragment.kt
+++ android-app/app/src/main/java/com/georgeflug/budget/view/transaction/edit/EnterDescriptionFragment.kt
@@ -1,12 +1,14 @@
 package com.georgeflug.budget.view.transaction.edit
 
 import android.app.Fragment
+import android.content.Context
 import android.os.Bundle
 import android.text.Editable
 import android.text.TextWatcher
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.view.inputmethod.InputMethodManager
 import com.georgeflug.budget.R
 import com.georgeflug.budget.util.FragmentUtil
 import kotlinx.android.synthetic.main.fragment_enter_description.*
@@ -25,6 +27,7 @@ class EnterDescriptionFragment : Fragment() {
 
         doneButton.setOnClickListener {
             isSuccess = true
+            hideKeyboard()
             FragmentUtil.popBackStackTo(FragmentUtil.EditDetailsWorkflowStack)
         }
 
@@ -40,4 +43,10 @@ class EnterDescriptionFragment : Fragment() {
 
         descriptionText.setText(description)
     }
+
+    private fun hideKeyboard() {
+        val imm = context.getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
+        imm.hideSoftInputFromWindow(view.getWindowToken(), 0)
+    }
+
 }
