c70e53b2ccf5c17e76e5136f486444faa00a5cfb
Richard White <georgeflug@gmail.com>
2019-04-23 19:11:13 -0500

    Add local host

--- android-app/app/src/main/java/com/georgeflug/budget/api/okhttp/CustomHostNameVerifier.kt
+++ android-app/app/src/main/java/com/georgeflug/budget/api/okhttp/CustomHostNameVerifier.kt
@@ -6,8 +6,9 @@ import javax.net.ssl.SSLSession
 class CustomHostNameVerifier : HostnameVerifier {
     private val localHost = "10.0.2.2"
     private val realHost = "georgeflug.duckdns.org"
+    private val realLocalNetworkHost = "192.168.1.132"
 
     override fun verify(hostname: String, session: SSLSession): Boolean {
-        return hostname == localHost || hostname == realHost
+        return hostname == localHost || hostname == realHost || hostname == realLocalNetworkHost
     }
 }
