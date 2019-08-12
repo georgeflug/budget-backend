f1d30da7aef41cbb8f46ed5e8d744480f775c7fc
Richard White <georgeflug@gmail.com>
2019-08-11 21:09:53 -0500

    Show green/red remaining budget on budget list. First budget list sort order.

---
+++ android-app/app/src/main/java/com/georgeflug/budget/view/budget/BudgetAdapter.kt
@@ -0,0 +1,89 @@
+package com.georgeflug.budget.view.budget
+
+import android.content.Context
+import android.graphics.Color
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.BaseAdapter
+import android.widget.ImageView
+import android.widget.TextView
+import com.georgeflug.budget.R
+import java.math.BigDecimal
+import java.text.NumberFormat
+
+class BudgetAdapter(val context: Context) : BaseAdapter() {
+    val currencyFormatter = NumberFormat.getCurrencyInstance().apply {
+        maximumFractionDigits = 0
+    }
+
+    var data: List<MonthCategoryRollup> = listOf()
+        set(value) {
+            field = sortBudgets(value)
+            notifyDataSetChanged()
+        }
+
+    override fun getView(position: Int, convertView: View?, parent: ViewGroup?): View {
+        val view = convertView
+                ?: LayoutInflater.from(context).inflate(R.layout.budget_item, parent, false)
+
+        val budget = data[position]
+        view.findViewById<ImageView>(R.id.budgetImage).setImageResource(budget.iconId)
+        view.findViewById<TextView>(R.id.budgetNameText).text = budget.title
+        view.findViewById<TextView>(R.id.remainingBudgetText).text = getRemainingToSpendText(budget)
+        view.findViewById<TextView>(R.id.remainingBudgetText).setTextColor(getRemainingToSpendColor(budget))
+        view.findViewById<TextView>(R.id.budgetAllocationText).text = getAllocatedText(budget)
+        view.findViewById<TextView>(R.id.budgetTotalText).text = getTotalAmountText(budget)
+
+        return view
+    }
+
+    override fun getItem(position: Int): Any {
+        return data[position]
+    }
+
+    override fun getItemId(position: Int): Long {
+        return position.toLong()
+    }
+
+    override fun getCount(): Int {
+        return data.size
+    }
+
+    private fun sortBudgets(budgets: List<MonthCategoryRollup>): List<MonthCategoryRollup> {
+        return budgets.sortedBy {
+            val prefix = if (it.perMonth == BigDecimal.ZERO) "1" else "0"
+            prefix + it.title
+        }
+    }
+
+    private fun getTotalAmountText(budget: MonthCategoryRollup): String {
+        return if (budget.total >= BigDecimal.ZERO) {
+            currencyFormatter.format(budget.total)
+        } else {
+            "+" + currencyFormatter.format(-budget.total)
+        }
+    }
+
+    private fun getRemainingToSpendText(budget: MonthCategoryRollup): CharSequence {
+        if (budget.perMonth == BigDecimal.ZERO) {
+            return ""
+        }
+
+        if (budget.remainingToSpend < BigDecimal.ZERO) {
+            val formattedAmount = currencyFormatter.format(-budget.remainingToSpend)
+            return "$formattedAmount over budget"
+        }
+        val formattedAmount = currencyFormatter.format(budget.remainingToSpend)
+        return "$formattedAmount remaining"
+    }
+
+    private fun getRemainingToSpendColor(budget: MonthCategoryRollup): Int {
+        return if (budget.remainingToSpend < BigDecimal.ZERO) Color.RED else Color.GREEN
+    }
+
+    private fun getAllocatedText(budget: MonthCategoryRollup): String {
+        val formattedAmount = currencyFormatter.format(budget.perMonth)
+        return if (budget.perMonth == BigDecimal.ZERO) "" else "($formattedAmount per month)"
+    }
+}
--- android-app/app/src/main/java/com/georgeflug/budget/view/budget/BudgetListFragment.kt
+++ android-app/app/src/main/java/com/georgeflug/budget/view/budget/BudgetListFragment.kt
@@ -5,14 +5,10 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.SimpleAdapter
-import com.georgeflug.budget.R
-import com.georgeflug.budget.model.Budget
 import com.georgeflug.budget.view.transaction.list.TransactionListFragment
 import kotlinx.android.synthetic.main.fragment_budget_list.*
-import java.math.BigDecimal
-import java.text.NumberFormat
 import java.time.LocalDate
+
 
 class BudgetListFragment : Fragment() {
     var month: LocalDate? = null
@@ -21,13 +17,12 @@ class BudgetListFragment : Fragment() {
             rePopulateBudgets()
         }
     private val budgets = BudgetModel()
-    private val visibleBudgets = mutableListOf<Map<String, String>>()
     lateinit var transactionsListFragment: TransactionListFragment
     private var isStopped = true
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
                               savedInstanceState: Bundle?): View? {
-        return inflater.inflate(R.layout.fragment_budget_list, container, false)
+        return inflater.inflate(com.georgeflug.budget.R.layout.fragment_budget_list, container, false)
     }
 
     override fun onStart() {
@@ -35,7 +30,7 @@ class BudgetListFragment : Fragment() {
 
         isStopped = false
 
-        setUpBudgetListAdapter()
+        budgetList.adapter = BudgetAdapter(context)
         rePopulateBudgets()
 
         budgets.setOnChangeListener(Runnable {
@@ -43,8 +38,8 @@ class BudgetListFragment : Fragment() {
         })
 
         budgetList.setOnItemClickListener { parent, _, position, _ ->
-            val dataMap = parent.adapter.getItem(position) as Map<String, String>
-            transactionsListFragment.filterBudget = Budget.lookupOrUnknown(dataMap["title"] ?: "")
+            val dataMap = parent.adapter.getItem(position) as MonthCategoryRollup
+            transactionsListFragment.filterBudget = dataMap.budget
             BudgetFragmentUtil.showAndAddToBackStack(transactionsListFragment)
         }
     }
@@ -55,51 +50,11 @@ class BudgetListFragment : Fragment() {
         budgets.setOnChangeListener(null)
     }
 
-    fun setUpBudgetListAdapter() {
-        val from = arrayOf("title", "allocated", "total", "iconId")
-        val to = intArrayOf(R.id.budgetNameText, R.id.budgetAllocationText, R.id.budgetTotalText, R.id.budgetImage)
-        budgetList.adapter = SimpleAdapter(context, visibleBudgets, R.layout.budget_item, from, to)
-    }
-
     fun rePopulateBudgets() {
         val theMonth = month
         if (theMonth == null || isStopped) {
             return
         }
-
-        val results = budgets.getMonth(theMonth).budgets.map { budget ->
-            mapOf(
-                    "title" to budget.title,
-                    "total" to getTotalAmountText(budget),
-                    "allocated" to getAllocatedText(budget),
-                    "iconId" to budget.iconId.toString()
-            )
-        }
-
-        val sorted = results.sortedBy {
-            val prefix = if (it["allocated"] == null) "1" else "0"
-            prefix + it["title"]
-        }
-
-        visibleBudgets.clear()
-        visibleBudgets.addAll(sorted)
-
-        (budgetList.adapter as SimpleAdapter).notifyDataSetChanged()
-    }
-
-    fun getTotalAmountText(budget: MonthCategoryRollup): String {
-        val currencyFormatter = NumberFormat.getCurrencyInstance()
-        return if (budget.total >= BigDecimal.ZERO) {
-            currencyFormatter.format(budget.total)
-        } else {
-            "+" + currencyFormatter.format(-budget.total)
-        }
-    }
-
-    fun getAllocatedText(budget: MonthCategoryRollup): String {
-        if (budget.perMonth == BigDecimal.ZERO) {
-            return ""
-        }
-        return "${budget.allocated} (${budget.perMonth} per month)"
+        (budgetList.adapter as BudgetAdapter).data = budgets.getMonth(theMonth).budgets.toList()
     }
 }
