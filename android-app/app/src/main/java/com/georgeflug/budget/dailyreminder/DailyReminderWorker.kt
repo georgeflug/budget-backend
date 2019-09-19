4c49332c08453e60b35b09b3ceb59cd8998c53d4
Richard White <georgeflug@gmail.com>
2019-09-18 19:47:42 -0500

    Add notification when failed to retrieve today's transactions, or when no new transactions are available.

--- android-app/app/src/main/java/com/georgeflug/budget/dailyreminder/DailyReminderWorker.kt
+++ android-app/app/src/main/java/com/georgeflug/budget/dailyreminder/DailyReminderWorker.kt
@@ -15,30 +15,49 @@ import com.georgeflug.budget.util.getNotificationManager
 class DailyReminderWorker(appContext: Context, workerParams: WorkerParameters)
     : Worker(appContext, workerParams) {
 
-    companion object {
-        val TAG = DailyReminderWorker::class.java.simpleName
-    }
-
     override fun doWork(): Result {
-        Log.d(TAG, "Running DailyReminderWorker");
-        val persistedTransactions = PersistedTransactionService.getPersistedTransactions()
-        val latestTimestamp = PersistedTransactionService.getLatestTimestamp(persistedTransactions)
-        BudgetApi.transactions.refreshTransactions().blockingSubscribe()
-        val newTransactions = BudgetApi.transactions.listTransactions(latestTimestamp).blockingFirst()
+        try {
+            Log.d("DailyReminderWorker", "Running DailyReminderWorker");
+            val persistedTransactions = PersistedTransactionService.getPersistedTransactions()
+            val latestTimestamp = PersistedTransactionService.getLatestTimestamp(persistedTransactions)
+            BudgetApi.transactions.refreshTransactions().blockingSubscribe()
+            val newTransactions = BudgetApi.transactions.listTransactions(latestTimestamp).blockingFirst()
 
-        val initialUncategorizedCount = persistedTransactions.count { it.isUncategorized() }
-        val newUncategorizedCount = newTransactions.count { it.isUncategorized() }
+            val initialUncategorizedCount = persistedTransactions.count { it.isUncategorized() }
+            val newUncategorizedCount = newTransactions.count { it.isUncategorized() }
 
-        Log.d(TAG, "Daily Reminder Results: $newUncategorizedCount new/$initialUncategorizedCount total uncategorized transactions");
-        if (newUncategorizedCount > 0) sendNotification(initialUncategorizedCount, newUncategorizedCount)
+            Log.d("DailyReminderWorker", "Daily Reminder Results: $newUncategorizedCount new/$initialUncategorizedCount total uncategorized transactions");
+            sendNotification(initialUncategorizedCount, newUncategorizedCount)
 
-        DailyReminderScheduler().scheduleReminder(applicationContext)
+            DailyReminderScheduler().scheduleReminder(applicationContext)
+        } catch (e: Exception) {
+            sendErrorNotification(e)
+        }
         return Result.success()
     }
 
     private fun sendNotification(initialUncategorizedCount: Int, newTransactions: Int) {
-        val message = "$newTransactions new transaction${if (newTransactions == 1) "" else "s"} to categorize " +
-                "and $initialUncategorizedCount old one${if (initialUncategorizedCount == 1) "" else "s"}"
+        val message = if (newTransactions == 0) {
+            "No new transactions to categorize " +
+                    "and $initialUncategorizedCount old one${if (initialUncategorizedCount == 1) "" else "s"}"
+        } else {
+            "$newTransactions new transaction${if (newTransactions == 1) "" else "s"} to categorize " +
+                    "and $initialUncategorizedCount old one${if (initialUncategorizedCount == 1) "" else "s"}"
+        }
+
+        // send notification
+        val notification = NotificationCompat.Builder(applicationContext, DailyReminderNotificationChannelInitializer.CHANNEL_ID)
+                .setContentTitle("New Transactions")
+                .setContentText(message)
+                .setPriority(NotificationCompat.PRIORITY_DEFAULT)
+                .setSmallIcon(R.drawable.ic_attach_money_black_24dp)
+                .build()
+
+        applicationContext.getNotificationManager().notify(1, notification)
+    }
+
+    private fun sendErrorNotification(e: Exception) {
+        val message = "Could not retrieve today's transactions: ${e.message}"
 
         // send notification
         val notification = NotificationCompat.Builder(applicationContext, DailyReminderNotificationChannelInitializer.CHANNEL_ID)
