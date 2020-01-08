4cc149f123e451176ea90e13267a0c088f6a6a3e
Richard White <georgeflug@gmail.com>
2020-01-07 21:07:14 -0600

    Change method order.

--- backend/src/log.ts
+++ backend/src/log.ts
@@ -1,15 +1,15 @@
 // temporary code to have typescript recognize this file as a module
 export {};
 
-function log(level, section, logText, exception = undefined) {
-  console.log(`${new Date().toISOString()} ${level} ${section}: ${logText}`);
-  if (exception) console.log(exception);
-}
-
 export function debug(section, logText, exception = undefined) {
   log('DEBUG', section, logText, exception);
 }
 
 export function error(section, logText, exception = undefined) {
   log('ERROR', section, logText, exception);
+}
+
+function log(level, section, logText, exception = undefined) {
+  console.log(`${new Date().toISOString()} ${level} ${section}: ${logText}`);
+  if (exception) console.log(exception);
 }
