97de554ab10192d72ffad2cb2ecefafb8750bf62
Richard White <georgeflug@gmail.com>
2020-05-12 21:32:42 -0500

    [Json Db] Remove _id field from transaction split during migration.

--- backend/src/debug-scripts/json-db-migration.ts
+++ backend/src/debug-scripts/json-db-migration.ts
@@ -11,10 +11,10 @@ import { RawPlaidV2 } from "../raw-plaid/raw-plaid-model";
 
 (async () => {
 
-  await connectToDbWithRetry('localhost');
+  await connectToDbWithRetry("localhost");
 
   const transactions = await listTransactions();
-  const jsonTransactionDb = new JsonDatabase<TransactionV2>('data/transactions');
+  const jsonTransactionDb = new JsonDatabase<TransactionV2>("data/transactions");
   for (const transaction of transactions) {
     const transactionV2: TransactionV2 = {
       account: transaction.account,
@@ -22,31 +22,35 @@ import { RawPlaidV2 } from "../raw-plaid/raw-plaid-model";
       plaidId: transaction.plaidId,
       postedDate: transaction.postedDate,
       postedDescription: transaction.postedDescription,
-      splits: transaction.splits,
+      splits: transaction.splits.map(split => ({
+        amount: split.amount,
+        budget: split.budget,
+        description: split.description,
+      })),
       totalAmount: transaction.totalAmount,
       createdAt: transaction.date,
       modifiedAt: transaction.updatedAt,
       recordId: 0, // will be overwritten by db layer
-      version: 0, // will be overwritten by db layer
+      version: 0 // will be overwritten by db layer
     };
     await jsonTransactionDb.createRecord(transactionV2);
   }
 
   const featureIdeas = await listFeatureIdeas();
-  const jsonFeatureIdeaDb = new JsonDatabase<FeatureIdeaV2>('data/feature-ideas');
+  const jsonFeatureIdeaDb = new JsonDatabase<FeatureIdeaV2>("data/feature-ideas");
   for (const idea of featureIdeas) {
     const ideaV2: FeatureIdeaV2 = {
       description: idea.description,
       createdAt: idea.date,
       modifiedAt: idea.date,
       recordId: 0, // will be overwritten by db layer
-      version: 0, // will be overwritten by db layer
+      version: 0 // will be overwritten by db layer
     };
     await jsonFeatureIdeaDb.createRecord(ideaV2);
   }
 
   const balances = await listBalances();
-  const balancesDb = new JsonDatabase<BalanceV2>('data/balances');
+  const balancesDb = new JsonDatabase<BalanceV2>("data/balances");
   for (const balance of balances) {
     const balanceV2: BalanceV2 = {
       accountId: balance.accountId,
@@ -61,7 +65,7 @@ import { RawPlaidV2 } from "../raw-plaid/raw-plaid-model";
   }
 
   const rawPlaids = await RawPlaidDbModel.find({}).exec();
-  const rawPlaidDb = new JsonDatabase<RawPlaidV2>('data/raw-plaid');
+  const rawPlaidDb = new JsonDatabase<RawPlaidV2>("data/raw-plaid");
   for (const rawPlaid of rawPlaids) {
     const rawPlaidV2: RawPlaidV2 = {
       data: rawPlaid.data,
@@ -73,7 +77,7 @@ import { RawPlaidV2 } from "../raw-plaid/raw-plaid-model";
     await rawPlaidDb.createRecord(rawPlaidV2);
   }
 
-  console.log('done!');
+  console.log("done!");
   process.exit(0);
 })();
 
