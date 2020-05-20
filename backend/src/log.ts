2272c1e5b8814c59da4edf2b9c38d0e3a802a122
Richard White <georgeflug@gmail.com>
2020-05-20 08:42:13 -0500

    [Json Db] Indent logged errors for more readable log file.

--- backend/src/log.ts
+++ backend/src/log.ts
@@ -2,11 +2,16 @@ export function debug(section, logText, exception = undefined) {
   log('DEBUG', section, logText, exception);
 }
 
-export function error(section, logText, exception = undefined) {
+export function error(section, logText, exception: (Error | undefined) = undefined) {
   log('ERROR', section, logText, exception);
 }
 
-function log(level, section, logText, exception = undefined) {
+function log(level, section, logText, exception: (Error | undefined) = undefined) {
   console.log(`${new Date().toISOString()} ${level} ${section}: ${logText}`);
-  if (exception) console.log(exception);
+  if (exception && exception.stack) console.log(indent(exception.stack));
+}
+
+function indent(text: string): string {
+  const indentation = ' '.repeat(4);
+  return indentation + text.replace(/\n/g, '\n' + indentation);
 }
