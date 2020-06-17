44dfb5198743e8cb3725bbbcacbf0bfcf9d68e3a
Richard White <georgeflug@gmail.com>
2020-06-17 09:39:06 -0500

    [Json Db] Fix files cached multiple times during asynchronous listing
    All files are retrieved several times simultaneously during transaction
    download. Normally the cache would handle this, but since the cache does
    not exist yet, it tries to cache all the files around 100 times at once.

--- backend/src/db/file-cache.ts
+++ backend/src/db/file-cache.ts
@@ -1,13 +1,20 @@
 import { promises as fs } from "fs";
 
+const PENDING = 'PENDING';
+
 export class FileCache {
   private cache = {};
 
   async readFile(path: string): Promise<string> {
-    const cached = this.cache[path];
+    let cached = this.cache[path];
     if (cached) {
+      while (cached === PENDING) {
+        await new Promise(setImmediate);
+        cached = this.cache[path];
+      }
       return cached;
     }
+    this.cache[path] = PENDING;
     const buffer = await fs.readFile(path);
     const data = buffer.toString();
     this.cache[path] = data;
