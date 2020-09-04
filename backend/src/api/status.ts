ed1e16a962cafc6066d675fa6935720d664ae875
Richard White <georgeflug@gmail.com>
2020-09-04 10:02:34 -0500

    [Web] Remove Hello World endpoint overriding static index.html

--- backend/src/api/status.ts
+++ backend/src/api/status.ts
@@ -1,8 +1,4 @@
 export function init(app) {
-  app.get('/', function (req, res) {
-    res.send('Hello World')
-  });
-
   app.get('/status', function (req, res) {
     res.send('{"status":"OK"}')
   });
