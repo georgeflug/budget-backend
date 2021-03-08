0cc08aaf4a86b71cf6d63eb3f37b57074ce3a7dc
Richard White <georgeflug@gmail.com>
2021-03-07 21:02:18 -0600

    Add test reporting during builds

--- jest.config.js
+++ jest.config.js
@@ -2,4 +2,9 @@ module.exports = {
   preset: 'ts-jest',
   testEnvironment: 'node',
   testPathIgnorePatterns: ['/out/'],
+  reporters: ['default', ['jest-junit', {
+      outputDirectory: './build',
+      outputName: 'jest-junit.xml',
+    }]
+  ],
 };
